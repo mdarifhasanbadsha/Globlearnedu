9c9993c662b3cf368588071aa753c26bd27bc5c9
Md Arif Hasan Badsha <90980461+mdarifhasanbadsha@users.noreply.github.com>
2026-05-03 04:15:24 +0800

    Delete app directory

--- app/sitemap.ts
+++
@@ -1,8 +0,0 @@
-
-export default function sitemap() {
-  return [
-    { url: "http://localhost:3000" },
-    { url: "http://localhost:3000/universities" },
-    { url: "http://localhost:3000/contact" }
-  ];
-}
